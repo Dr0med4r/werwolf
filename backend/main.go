ea6b3e4ce1b56aa7f84fa53b17c313dfda432844
wmneco <>
2024-12-30 17:33:51 +0100

    Changed backend code infrastructure

--- backend/main.go
+++ backend/main.go
@@ -1,23 +1,20 @@
 package main
 
 import (
-	"errors"
+	"encoding/json"
 	"fmt"
-	"html/template"
+	"io"
 	"log"
-	"math/rand"
 	"net/http"
-	"slices"
-	"strconv"
 )
 
-var templates = template.Must(template.ParseFiles(
-	"edit.html",
-	"view.html",
-	"index.html",
-	"newGame.html",
-	"Game.html",
-))
+// var templates = template.Must(template.ParseFiles(
+// 	"edit.html",
+// 	"view.html",
+// 	"index.html",
+// 	"newGame.html",
+// 	"Game.html",
+// ))
 
 type Game struct {
 	Cards   *[]Card
@@ -30,126 +27,175 @@ type Card struct {
 	Name string
 }
 
+type NewGameRequest struct {
+	GameCode int
+}
+
+type StartRequest struct {
+	GameCode int
+}
+
+type NewCardRequest struct {
+	GameCode int
+	CardName string
+}
+
 var games map[int]Game = make(map[int]Game)
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "index", nil)
-}
+// func rootHandler(w http.ResponseWriter, r *http.Request) {
+// 	renderTemplate(w, "index", nil)
+// }
 
 func newGameHandler(w http.ResponseWriter, r *http.Request) {
-	code, err := getCode(r)
+
+	fmt.Println("NEW: game request")
+	var n NewGameRequest
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+
+	body, err := io.ReadAll(r.Body)
 	if err != nil {
-		http.Redirect(w, r, "/", http.StatusBadRequest)
+		http.Error(w, "Error reading request body", http.StatusInternalServerError)
 		return
 	}
 
-	if games[code].Cards == nil {
-		games[code] = Game{Cards: new([]Card), Code: code}
+	if err := json.Unmarshal(body, &n); err != nil {
+		http.Error(w, "Error parsing request body", http.StatusBadRequest)
+		return
 	}
 
-	renderTemplate(w, "newGame", games[code])
+	if games[n.GameCode].Cards == nil {
+		games[n.GameCode] = Game{Cards: new([]Card), Code: n.GameCode}
+	}
+
+	json.NewEncoder(w).Encode(games[n.GameCode])
 }
 
-func playHandler(w http.ResponseWriter, r *http.Request) {
-	code, err := getCode(r)
-	if err != nil || !r.URL.Query().Has("name") {
-		http.Redirect(w, r, "/", http.StatusBadRequest)
-		return
-	}
-	if games[code].Players == nil || games[code].Cards == nil {
-		http.Redirect(w, r, fmt.Sprint("/game/?code=%i", code), http.StatusBadRequest)
-		return
+// func playHandler(w http.ResponseWriter, r *http.Request) {
+// 	code, err := getCode(r)
+// 	if err != nil || !r.URL.Query().Has("name") {
+// 		http.Redirect(w, r, "/", http.StatusBadRequest)
+// 		return
+// 	}
+// 	if games[code].Players == nil || games[code].Cards == nil {
+// 		http.Redirect(w, r, fmt.Sprint("/game/?code=%i", code), http.StatusBadRequest)
+// 		return
 
-	}
+// 	}
 
-	name := r.URL.Query().Get("name")
-	moderator := r.URL.Query().Has("moderator")
-	taken := make([]int, 0, len(*games[code].Cards))
-	for _, v := range games[code].Players {
-		taken = append(taken, v)
-	}
-	available := make([]int, 0, len(*games[code].Cards))
-	for i := range *games[code].Cards {
-		if !slices.Contains(taken, i) {
-			available = append(available, i)
-		}
-	}
+// 	name := r.URL.Query().Get("name")
+// 	moderator := r.URL.Query().Has("moderator")
+// 	taken := make([]int, 0, len(*games[code].Cards))
+// 	for _, v := range games[code].Players {
+// 		taken = append(taken, v)
+// 	}
+// 	available := make([]int, 0, len(*games[code].Cards))
+// 	for i := range *games[code].Cards {
+// 		if !slices.Contains(taken, i) {
+// 			available = append(available, i)
+// 		}
+// 	}
 
-	if !moderator {
-		_, exists := games[code].Players[name]
-		if len(available) == 0 && !exists {
-			http.Redirect(w, r, fmt.Sprintf("/game/?code=%d", code), http.StatusFound)
-			return
-		}
-		if !exists {
-			games[code].Players[name] = available[rand.Intn(len(available))]
-		}
-	}
+// 	if !moderator {
+// 		_, exists := games[code].Players[name]
+// 		if len(available) == 0 && !exists {
+// 			http.Redirect(w, r, fmt.Sprintf("/game/?code=%d", code), http.StatusFound)
+// 			return
+// 		}
+// 		if !exists {
+// 			games[code].Players[name] = available[rand.Intn(len(available))]
+// 		}
+// 	}
 
-	renderTemplate(w, "Game", struct {
-		Player    string
-		Game      Game
-		Moderator bool
-	}{Player: name, Game: games[code], Moderator: moderator})
+// 	renderTemplate(w, "Game", struct {
+// 		Player    string
+// 		Game      Game
+// 		Moderator bool
+// 	}{Player: name, Game: games[code], Moderator: moderator})
 
-}
+// }
+
 func startHandler(w http.ResponseWriter, r *http.Request) {
-	code, err := getCode(r)
-	if err != nil {
-		http.Redirect(w, r, "/", http.StatusBadRequest)
+	fmt.Println("NEW: game start request")
+	var n StartRequest
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
-	game := games[code]
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusInternalServerError)
+		return
+	}
+
+	if err := json.Unmarshal(body, &n); err != nil {
+		http.Error(w, "Error parsing request body", http.StatusBadRequest)
+		return
+	}
+
+	game := games[n.GameCode]
 	game.Started = true
 	game.Players = make(map[string]int)
-	games[code] = game
-	http.Redirect(w, r, fmt.Sprintf("/game?code=%d", code), http.StatusFound)
+	games[n.GameCode] = game
+	w.WriteHeader(http.StatusOK)
 }
 
 func cardHandler(w http.ResponseWriter, r *http.Request) {
-	code, err := getCode(r)
-	if err != nil || !r.URL.Query().Has("card") {
-		http.Redirect(w, r, "/", http.StatusBadRequest)
+	fmt.Println("NEW: card request")
+	var n NewCardRequest
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
-	card := Card{r.URL.Query().Get("card")}
-
-	if card.Name == "" || games[code].Cards == nil {
-		http.Redirect(w, r, fmt.Sprintf("/game/?code=%d", code), http.StatusFound)
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusInternalServerError)
 		return
 	}
-	*games[code].Cards = append(*games[code].Cards, card)
-	http.Redirect(w, r, fmt.Sprintf("/game/?code=%d", code), http.StatusFound)
+
+	if err := json.Unmarshal(body, &n); err != nil {
+		http.Error(w, "Error parsing request body", http.StatusBadRequest)
+		return
+	}
+
+	card := Card{n.CardName}
+
+	if card.Name == "" || games[n.GameCode].Cards == nil {
+		http.Error(w, "No Content", http.StatusNoContent)
+		return
+	}
+
+	*games[n.GameCode].Cards = append(*games[n.GameCode].Cards, card)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(*(games[n.GameCode].Cards))
 
 }
 
-func getCode(r *http.Request) (int, error) {
-	var code int
-	if !r.URL.Query().Has("code") {
-		return 0, errors.New("no Code")
-	}
-	code, err := strconv.Atoi(r.URL.Query().Get("code"))
-	if err != nil {
-		return 0, err
-	}
-	return code, nil
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string, p any) {
-	err := templates.ExecuteTemplate(w, tmpl+".html", p)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
+// func getCode(r *http.Request) (int, error) {
+// 	var code int
+// 	if !r.URL.Query().Has("code") {
+// 		return 0, errors.New("no Code")
+// 	}
+// 	code, err := strconv.Atoi(r.URL.Query().Get("code"))
+// 	if err != nil {
+// 		return 0, err
+// 	}
+// 	return code, nil
+// }
 
 func main() {
-	http.HandleFunc("/", rootHandler)
-	http.HandleFunc("/game/", newGameHandler)
-	http.HandleFunc("/addCard/", cardHandler)
-	http.HandleFunc("/playing/", playHandler)
-	http.HandleFunc("/startGame/", startHandler)
+	http.HandleFunc("/api/game/", newGameHandler)
+	http.HandleFunc("/api/card/", cardHandler)
+	// http.HandleFunc("/api/playing/", playHandler)
+	http.HandleFunc("/api/startGame/", startHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
--- frontend/frontend.Dockerfile
+++ frontend/frontend.Dockerfile
@@ -1,2 +1,3 @@
 FROM nginx:1.27
+COPY nginx.conf /etc/nginx/nginx.conf
 COPY . /usr/share/nginx/html
